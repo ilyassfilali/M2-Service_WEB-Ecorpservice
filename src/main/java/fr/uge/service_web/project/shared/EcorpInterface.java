188018ff7cf4c806d398962c97cf3d514afcc178
ilyassfilali <ilyassfilali6@gmail.com>
2021-12-04 16:44:13 +0100

    update_ecropinterface

--- src/main/java/fr/uge/service_web/project/shared/EcorpInterface.java
+++ src/main/java/fr/uge/service_web/project/shared/EcorpInterface.java
@@ -10,7 +10,8 @@ public interface EcorpInterface extends IfShareInterface{
 	public List<IOffer> getoffersbyuser(String iduser) throws RemoteException;
 	public List<IOffer> getoffersbyproduct(String idproduct) throws RemoteException;
 	
-	boolean connect(String email, String passwd) throws RemoteException;
+	public boolean connect(String email, String passwd) throws RemoteException;
+	public void addPasswd(String iduser,String passwd) throws RemoteException;
 	
 	public void addpushase(String iduser,String idoffre,int qt) throws RemoteException;
 	public List<IPurchase> getpurshasebyuser(String iduser) throws RemoteException;
