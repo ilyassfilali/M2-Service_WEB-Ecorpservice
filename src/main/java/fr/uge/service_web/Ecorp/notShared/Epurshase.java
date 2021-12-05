d51b3bf12beb55c629aaa18199859743989b04e0
ilyassfilali <ilyassfilali6@gmail.com>
2021-12-05 21:42:48 +0100

    update Epurshase

--- src/main/java/fr/uge/service_web/Ecorp/notShared/Epurshase.java
+++ src/main/java/fr/uge/service_web/Ecorp/notShared/Epurshase.java
@@ -1,9 +1,11 @@
 package fr.uge.service_web.Ecorp.notShared;
 
 import java.rmi.RemoteException;
+import java.util.Date;
 
 import fr.uge.service_web.ifshare.shared.IOffer;
 import fr.uge.service_web.ifshare.shared.IPurchase;
+import fr.uge.service_web.ifshare.shared.IUser;
 import fr.uge.service_web.ifshare.shared.PurchaseStatus;
 
 public class Epurshase implements IPurchase{
@@ -11,11 +13,15 @@ public class Epurshase implements IPurchase{
 	private IOffer offer;
 	private int qt;
 	private PurchaseStatus status;
+	private IUser buyer;
+	private Date date;
 	
-	public Epurshase(IOffer offer, int qt, PurchaseStatus status) {
+	public Epurshase(IOffer offer, int qt, PurchaseStatus status,IUser buyer,Date date) {
 		super();
 		this.offer = offer;
 		this.qt = qt;
+		this.buyer = buyer;
+		this.date = date;
 		this.status = status;
 	}
 
@@ -35,6 +41,18 @@ public class Epurshase implements IPurchase{
 	public PurchaseStatus getStatus() {
 		// TODO Auto-generated method stub
 		return status;
+	}
+
+	@Override
+	public IUser getBuyer() throws RemoteException {
+		// TODO Auto-generated method stub
+		return this.buyer;
+	}
+
+	@Override
+	public Date getPurchaseDate() throws RemoteException {
+		// TODO Auto-generated method stub
+		return date;
 	}
 
 }
