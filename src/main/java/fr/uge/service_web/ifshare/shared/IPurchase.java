3b0073aa5d04d76fc0be9184095dbdf0852bb9f6
ilyassfilali <ilyassfilali6@gmail.com>
2021-12-05 21:36:41 +0100

    update

--- src/main/java/fr/uge/service_web/ifshare/shared/IPurchase.java
+++ src/main/java/fr/uge/service_web/ifshare/shared/IPurchase.java
@@ -3,11 +3,16 @@ package fr.uge.service_web.ifshare.shared;
 import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
 
 public interface IPurchase extends Remote {
+	IUser getBuyer() throws RemoteException;
+
     IOffer getOffer() throws RemoteException;
 
-    int getQuantity();
+    int getQuantity() throws RemoteException;
 
-    PurchaseStatus getStatus();
+    PurchaseStatus getStatus() throws RemoteException;
+
+    Date getPurchaseDate() throws RemoteException;
 }
